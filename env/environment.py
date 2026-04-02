5490970e55540512e33390cbd986cbbb90dda1d2
Anubhav Singh Chouhan <anubhavsingh3040@gmail.com>
2026-04-02 16:51:26 +0530

    Refactor observation creation in environment.py

--- env/environment.py
+++ env/environment.py
@@ -14,7 +14,7 @@ class IncidentEnv:
             incident_id=self.index,
             logs=task["logs"],
             alert=task["alert"]
-        ).model_dump()   
+        )
 
     def step(self, action):
         task = self.tasks[self.index]
@@ -35,12 +35,12 @@ class IncidentEnv:
                 incident_id=self.index,
                 logs=next_task["logs"],
                 alert=next_task["alert"]
-            ).model_dump()  
+            )
         else:
-            obs = {
-                "incident_id": self.index,
-                "logs": "completed",
-                "alert": "done"
-            }
+            obs = Observation(
+                incident_id=self.index,
+                logs="completed",
+                alert="done"
+            )
 
-        return obs, Reward(score=score).model_dump(), done, {} 
+        return obs, Reward(score=score), done, {}
