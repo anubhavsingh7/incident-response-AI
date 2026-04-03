90400c3afda8025ac6e45f8e76efe951ef1cd628
Anubhav Singh Chouhan <anubhavsingh3040@gmail.com>
2026-04-03 12:20:28 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -58,3 +58,10 @@ async def state():
 def main():
     port = int(os.environ.get("PORT", 7860))
     uvicorn.run("server.app:app", host="0.0.0.0", port=port)
+
+def main():
+    port = int(os.environ.get("PORT", 7860))
+    uvicorn.run("server.app:app", host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
